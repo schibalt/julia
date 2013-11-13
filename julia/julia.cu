509b38d165b4975c6cae8d71675390c17b23d2fc
andrew <andrewtilisky@gmail.com>
2013-11-13 01:40:50 -0500

    done...?

--- julia/julia.cu
+++ julia/julia.cu
@@ -7,11 +7,15 @@
 #include <iostream>
 //#include <cutil.h>
 #include "cpu_bitmap.h"
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
 using std::cout;
 using std::endl;
 
-#define DIM 700
+#define XDIM 700
+#define YDIM 700
 
 struct cuComplex {
 
@@ -23,29 +27,45 @@ struct cuComplex {
 	__device__ cuComplex( float a, float b) : r(a), i(b) {}
 
 	__device__ float magnitude2(void) {
-		return r*r + i*i;
+		return r * r + i * i;
 	}
 
 	__device__ cuComplex operator*(const cuComplex& a) {
-		return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
+		return cuComplex(r * a.r - i * a.i, i * a.r + r * a.i);
 	}
 
 	__device__ cuComplex operator+(const cuComplex& a) {
-		return cuComplex(r+a.r, i+a.i);
+		return cuComplex(r + a.r, i + a.i);
+	}
+
+	__device__ cuComplex operator-(const cuComplex& a) {
+		return cuComplex(r - a.r, i - a.i);
 	}
 };
 
-__device__ int function(int x, int y,int set) {
+__device__ cuComplex magnitude2(cuComplex a, cuComplex c){
+	return a * a + c;
+}
+
+__device__ cuComplex magnitude3(cuComplex a, cuComplex c){
+	return a * a * a + c;
+}
+
+__device__ cuComplex braunlformula(cuComplex a, cuComplex c){
+	return a * a * a + c * a - c;
+}
+
+__device__ int function(int x, int y, int set, unsigned char magnitude, bool usebraunlformula, bool mandelbrot) {
 
 	const float scale = 1.5;
-	float jx = scale * (float) (DIM / 2 - x) / (DIM / 2);
-	float jy = scale * (float) (DIM / 2 - y) / (DIM / 2);
+	float jx = scale * (float) (XDIM / 2 - x) / (XDIM / 2);
+	float jy = scale * (float) (YDIM / 2 - y) / (YDIM / 2);
 
 	cuComplex c;
 
 	switch (set) {
 	case 0:
-		c.r=-.8;
+		c.r = -.8;
 		c.i = .156;
 		//= cuComplex(-0.8, 0.156);
 		break;
@@ -75,19 +95,40 @@ __device__ int function(int x, int y,int set) {
 		//c = cuComplex(-0.391, 0.587);
 		break;
 	}
-	cuComplex a(jx, jy);
 
-	//return a.magnitude2() % 8;
+	//cuComplex a(jx, jy);
+	cuComplex a;
+
+	int magfloor = 2;
+	float magceiling = 3;
+
+	cuComplex (*formula)(cuComplex, cuComplex);
+
+	if(magnitude == 2)
+		formula = magnitude2;
+	else if(usebraunlformula)
+		formula = braunlformula;
+	else
+		formula = magnitude3;
+
+	if(mandelbrot){
+		magfloor = 4;
+		a.r = 0;
+		a.i = 0;
+		c.r = jx;
+		c.i = jy;
+	}
+	else{
+		a.r = jx;
+		a.i = jy;
+		a = formula(a, c);
+	}
 
 	for (int i = 0; i < 47; i++) {
-
-		a = a * a + c;
 
 		int colors = 7;
 		for(int color = 0; color <= colors; color++){
 			float weight = (float)(colors - color) / colors;
-			float magfloor = 1.5;
-			float magceiling = 1000;
 			float threshhold = ((magceiling - magfloor) * weight) + magfloor;
 			//float threshhold = magceiling;
 
@@ -96,18 +137,20 @@ __device__ int function(int x, int y,int set) {
 			if (magnitude2 > threshhold )
 				return color;
 		}
+
+		a = formula(a, c);
 	}
 	return 7;
 }
 
-__global__ void kernel(unsigned char *ptr,int set) {
+__global__ void kernel(unsigned char *ptr, unsigned char set, unsigned char magnitude, unsigned char usebraunlformula, unsigned char mandelbrot) {
 	// map from threadIdx/blockIdx to pixel position
 	int x = blockIdx.x;
 	int y = blockIdx.y;
 	int offset = x + y * gridDim.x;
 
 	// now calculate the value at that position
-	int value = function(x, y,set);
+	int value = function(x, y, set, magnitude, usebraunlformula, mandelbrot);
 
 	int red, green, blue;
 
@@ -178,17 +221,18 @@ __global__ void kernel(unsigned char *ptr,int set) {
 
 int main(int argc, char* argv[]) {
 
-	int width = 750;
-	int height = 550;
-	CPUBitmap bitmap(width, height);
+	CPUBitmap bitmap(XDIM, YDIM);
 	unsigned char *dev_bitmap;
 
 	cudaMalloc((void**)&dev_bitmap, bitmap.image_size());
-	dim3 grid(width, height);
+	dim3 grid(XDIM, YDIM);
 
-	int set = atoi(argv[1]);
+	unsigned char set = atoi(argv[1]);
+	unsigned char magnitude = atoi(argv[2]);
+	unsigned char usebraunlformula = atoi(argv[3]);
+	unsigned char mandelbrot = atoi(argv[4]);
 
-	kernel<<<grid,1>>>(dev_bitmap,set);
+	kernel<<<grid,1>>>(dev_bitmap, set, magnitude, usebraunlformula, mandelbrot);
 
 	cudaMemcpy(bitmap.get_ptr(), dev_bitmap, 
 		bitmap.image_size(), 
